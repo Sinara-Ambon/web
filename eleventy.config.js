904b4bf666267b276c008a096c107958802acaa2
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2024-10-08 17:49:31 +0900

    Fix links to incorrect domain

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./assets/images");
   eleventyConfig.addPassthroughCopy("favicon.png");
+  eleventyConfig.addPassthroughCopy("CNAME");
 };
