ea8555a48a7f27f6e31c729d53e10ba44fc4ab6e
EmptyWork <22065214+EmptyWork@users.noreply.github.com>
2025-07-08 11:06:45 +0900

    Update filter to be more specific

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,6 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
 
   eleventyConfig.addFilter('normalizedUrl', url => {
-    return url.replace("sinara-web", "")
+    return url.replace("/sinara-web", "")
   })
 };
